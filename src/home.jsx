8c397e4122d8c997ad4a89e4163074978b84f857
Andrea Agustina Castillo <andreacastillo2022@gmail.com>
2026-09-22 21:56:42 -0300

    windows 95 home

--- src/home.jsx
+++ src/home.jsx
@@ -5,53 +5,98 @@ import { BiSolidSmile } from "react-icons/bi";
 import { FaHandshake } from "react-icons/fa6";
 import { LiaCertificateSolid } from "react-icons/lia";
 import { PiGearSixBold } from "react-icons/pi";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 
 function Home() {
-  
 
-  return (
+//*que la ventana se mueva*//  
+const [aboutOpen, setAboutOpen]= useState(true);
+const [position, setPosition] = useState({
+  x: 250,
+  y: 80
+});
+const [dragging, setDragging] = useState(false);
+
+const [offset, setOffset] = useState({
+  x: 0,
+  y: 0
+});
+
+useEffect(() => {
+  const handleMouseMove = (e) => {
+    if (!dragging) return;
+
+    setPosition({
+      x: e.clientX - offset.x,
+      y: e.clientY - offset.y
+    });
+  };
+
+  const handleMouseUp = () => {
+    setDragging(false);
+  };
+
+  window.addEventListener("mousemove", handleMouseMove);
+  window.addEventListener("mouseup", handleMouseUp);
+
+return () => {
+    window.removeEventListener("mousemove", handleMouseMove);
+    window.removeEventListener("mouseup", handleMouseUp);
+  };
+}, [dragging, offset]);
+
+
+return (
+
+  <main className="desktop">
     <>
-      <header>
-        <nav>
-          <ul className="nav">
-            <li>
-            <a href="#sobremi">
-              <i className='contenedor-i'><BiSolidSmile /></i>
-              <div className="circle"></div>
-              <div className ="titulos">Sobre mi</div>
-            </a>
-            </li>
+        <div className="desktop-icons">
 
-            <li>
-            <a href="#projects">
-              <i className='contenedor-i'><PiGearSixBold /></i>
-              <div className="circle"></div>
-              <div class ="titulos">Projectos</div>
-            </a>
-            </li>
+          <Link className="desktop-icon" to="/projects">
+            <PiGearSixBold />
+            <span>Proyectos</span>
+          </Link>
 
-            <li>
-            <a href="#certificados">
-              <i className='contenedor-i'><LiaCertificateSolid /></i>
-              <div className="circle"></div>
-              <div className ="titulos">certificados</div>
-            </a>
-            </li>
+          <Link className="desktop-icon" to="/projects">
+            <LiaCertificateSolid />
+            <span>Certificados</span>
+          </Link>
 
-            <li>
-            <a href="#contacto">
-              <i className='contenedor-i'><FaHandshake /></i>
-              <div className="circle"></div>
-              <div className ="titulos">contacto</div>
-            </a>
-            </li>
-            
-          </ul>
-        </nav>
-      </header>
+          <Link className="desktop-icon" to="/contact">
+            <FaHandshake />
+            <span>Contacto</span>
+          </Link>
+
+          </div>
+ 
+    {aboutOpen&& (
+      <div className="window about-window" 
+      style={{
+      left: `${position.x}px`,
+      top: `${position.y}px`}}
+
+      onMouseDown={(e) => {
+      setDragging(true);
+      setOffset({
+        x: e.clientX - position.x,
+        y: e.clientY - position.y});}}>
+
+  
+        <div className="window-titlebar">
+          <span>👤 Sobre mí</span>
+
+          <div className="window-buttons">
+            <button>_</button>
+            <button>□</button>
+            <button onClick={() => setAboutOpen(false)}>×</button>
+            </div>
+      </div>
+
 
       <section className="acercade" id="presentation">
+
         <div className="acercade-texto">
           <h1>Andrea Agustina Castillo</h1>
           <span className="tag">SOBRE MÍ</span>
@@ -100,15 +145,23 @@ function Home() {
             >
 
           <div className="decoración-foto"></div>
-
           <img src={andrea} alt="Foto de Andrea" />
 
-
         </div>
-      
       </section>
-    </>
-    
-      )}
+      </div>)}
 
+      <div className="taskbar">
+          <button className="start-button">
+            🪟 Start
+          </button>
+
+          <div className="taskbar-time">
+            21:00
+          </div>
+      </div>
+
+  </>
+  </main>
+)}
 export default Home
